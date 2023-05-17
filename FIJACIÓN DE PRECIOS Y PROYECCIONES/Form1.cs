a4946c34196280121672af7d05348c024ab2cfee
Pawel <Pawel@LAPTOPHP_OMEN>
2023-05-17 10:28:50 -0600

    Agregar archivos de proyecto.

---
+++ FIJACIÓN DE PRECIOS Y PROYECCIONES/Form1.cs
@@ -0,0 +1,10 @@
+namespace FIJACIÓN_DE_PRECIOS_Y_PROYECCIONES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
